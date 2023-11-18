17a3e4f0f195881dd45606c2ca8670f450f969fa
Mohd Zia <ansarizia9335@gmail.com>
2023-11-18 23:47:06 +0530

    connected with api

---
+++ api.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+# from PIL import Image
+# import numpy as np
+# import torch
+#from for_test_V20 import for_test
+
+app = Flask(__name__)
+
+# def preprocess_image(img):
+#     img = img.convert('L')
+#     img_array = np.array(img)
+#     img_tensor = torch.from_numpy(img_array).type(torch.FloatTensor)
+#     img_tensor = img_tensor / 255.0
+#     img_tensor = img_tensor.unsqueeze(0)
+#     img_tensor = img_tensor.unsqueeze(0)
+#     return img_tensor
+
+def predict_expression(img_tensor):
+    # Assuming for_test is defined and imported correctly
+    # attention, prediction = for_test(img_tensor)
+    
+    prediction_string = 'Surprise MMMMMother fucker'
+    # img_array = np.array(img_tensor.squeeze(0).squeeze(0))
+    
+    # for i in range(attention.shape[0]):
+    #     if i >= len(prediction):
+    #         break
+    #     elif prediction[i] == '<eol>':
+    #         continue
+    #     else:
+    #         prediction_string += prediction[i]
+
+    return prediction_string
+
+@app.route('/', methods=['POST'])
+def predict():
+    try:
+        # Get the image file from the POST request
+        # print(request.files)
+        print("api - api")
+        # img_file = request.files['image']
+        # img = Image.open(img_file)
+        
+        # # Preprocess the image
+        # img_tensor = preprocess_image(img)
+        
+        # Make prediction
+        # prediction_string = predict_expression(img_tensor)
+        prediction_string = predict_expression(' ')
+        
+        # return jsonify({'prediction': prediction_string, 'message': 'Prediction saved to output.txt'})
+        return jsonify({'prediction': prediction_string})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+        
+
+if __name__ == '__main__':
+    app.run(port=5000)
--- app.js
+++ app.js
@@ -2,16 +2,30 @@ const express=require("express")
 const bodyParser=require("body-parser");
 const multer=require("multer");
 const Jimp = require('jimp');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
 
 
 
 const app = express();
 const port = process.env.PORT || 3000;
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
-app.use(express.urlencoded( { extended: true } ));
-app.use(express.static('public'));
+
+// Multer upload
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
+
+app.use(express.static(__dirname + '/public'));
+app.use('/uploads', express.static('uploads'));
 
 
 app.get("/",(req,res)=>{
@@ -19,28 +33,46 @@ app.get("/",(req,res)=>{
     
 })
 
-app.post("/", upload.single('image'), async (req, res) => {
+app.post("/", upload.single('input_image'), async (req, res) => {
     if (!req.file) {
         res.status(400).send("No image uploaded");
         return;
     }
 
     try {
-        const imageBuffer = req.file.buffer;
-        const image = await Jimp.read(imageBuffer);
-
-        // Convert the image to BMP
-        const bmpBuffer = await image.getBufferAsync(Jimp.MIME_BMP);
-
-        // Send the BMP image as a response
-        res.set({
-            'Content-Type': 'image/bmp',
-            'Content-Disposition': 'attachment; filename=image.bmp'
+        // Make a POST request to the Flask API using http.request
+        const options = {
+            hostname: '127.0.0.1',
+            port: 5000,
+            path: '/',
+            method: 'POST',
+        };
+        const apiReq = http.request(options, (apiRes) => {
+            let data = '';
+    
+            apiRes.on('data', (chunk) => {
+                data += chunk;
+            });
+    
+            apiRes.on('end', () => {
+                prediction = JSON.parse(data);
+                console.log('Body:', prediction);
+                
+                // Send the response to the client here
+                res.status(200).send("<h1>"+prediction.prediction+"</h1>" );
+            });
         });
-        res.send(bmpBuffer);
+    
+        apiReq.on("error", (err) => {
+            console.log("Error: ", err);
+        });
+    
+        // End the request
+        apiReq.end();
+        
     } catch (error) {
-        console.error("Error processing the image:", error);
-        res.status(500).send("Error processing the image");
+        console.error('Error processing the image:', error);
+        res.status(500).send('Internal Server Error');
     }
 });
 app.listen(port,()=>{
