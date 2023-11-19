c037f4a2a4a2ffa32502479d9d70bedac4fff10d
SK-ILLish-GIT <sksahilparvez2000@gmail.com>
2023-11-19 16:20:07 +0530

    OAuth

---
+++ public/profileOAuth.js
@@ -0,0 +1,65 @@
+let params = {};
+let regExp = /([^&=]+)=([^&]*)/g,
+  m;
+while ((m = regExp.exec(location.href))) {
+  params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+}
+if (Object.keys(params).length !== 0) {
+  localStorage.setItem("authInfo", JSON.stringify(params));
+  localStorage.setItem("userName", params["name"]);
+  localStorage.setItem("userEmail", params["email"]);
+  localStorage.setItem("profilePic",params["prfileImage"])
+}
+//hide access token
+window.history.pushState({}, document.title, "/" + "profile.html");
+
+let info = JSON.parse(localStorage.getItem("authInfo"));
+
+// console.log(info);
+// console.log(info['access_token']);
+// console.log(info['expires_in']);
+
+fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
+  headers: {
+    Authorization: `Bearer ${info["access_token"]}`,
+  },
+})
+  .then((response) => response.json())
+  .then((userInfo) => {
+    // console.log(userInfo);
+    document.getElementById("name").innerHTML += userInfo.name;
+    document.getElementById("mail").innerHTML += userInfo.email;
+    document
+      .getElementById("profileImage")
+      .setAttribute("src", userInfo.picture);
+
+    // Store name and email in local storage
+    localStorage.setItem("userName", userInfo.name);
+    localStorage.setItem("userEmail", userInfo.email);
+    localStorage.setItem("profilePic",userInfo.picture);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+function logOut() {
+  fetch(
+    "https://accounts.google.com/o/oauth2/revoke?token=" + info["access_token"],
+    {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    }
+  )
+    .then((info) => {
+      // clear local storage
+      localStorage.clear();
+      localStorage.setItem("authInfo", null);
+      // redirect to index.html
+      window.location.href = "http://127.0.0.1:3000/";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
--- public/script.js
+++ public/script.js
@@ -26,13 +26,36 @@ document.addEventListener('DOMContentLoaded', function () {
             <!-- Right-aligned buttons -->
             <ul class="navbar-nav ml-auto p-10">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Sign Up</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <button id="signUp" onclick="signIn()">
+                        <img src="/images/logo/gLogo.png" alt="Google Logo" style="height: 25px; margin-right: 10px;">
+                    </button>
                 </li>
             </ul>
         </nav>
     `;
     navbarContainer.innerHTML = navbarHTML;
 });
+function signIn(){
+    let oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+    let form = document.createElement('form');
+    form.setAttribute('method', 'GET');
+    form.setAttribute('action', oauth2Endpoint);
+    let params = {
+        'client_id': '895922906167-i2pe2g5s8m57oi8gjgolahpuqc63aus7.apps.googleusercontent.com',
+        'redirect_uri': 'http://127.0.0.1:3000/profile.html',
+        'response_type': 'token',
+        'scope': 'openid https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+        'include_granted_scopes': 'true',
+        'state': 'pass-through value'
+    };
+    for(var p in params){
+        let input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
+    document.body.appendChild(form);
+    form.submit();
+}
